ef357c718ebede69655a1d023f98e02d90a3417f
Anastasiya <33719990+Anastasiya199889911@users.noreply.github.com>
2019-05-30 00:06:31 +0300

    set

--- wdg/settings.py
+++ wdg/settings.py
@@ -10,12 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-import os,sys
+import os, sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__) + "../../../")
-sys.path.append("..")
+# sys.path.append("..")
 
 
 # Quick-start development settings - unsuitable for production
@@ -127,4 +127,5 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, "static")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 STATIC_URL = '/static/'
+
 MEDIA_URL = '/media/'
