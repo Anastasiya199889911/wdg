44bd0ff704b15abc066d76897b29e95fde1c061f
Anastasiya199889911 <romanenko.anastasiya64@gmail.com>
2019-08-14 12:25:34 +0300

    редактирование главной страницы

--- static/js/main.js
+++ static/js/main.js
@@ -1,22 +1,22 @@
-$(function(){
-  $('a[href^="#"]').on('click', function(event) {
-    // отменяем стандартное действие
-    event.preventDefault();
-
-    var sc = $(this).attr("href"),
-        dn = $(sc).offset().top;
-    /*
-    * sc - в переменную заносим информацию о том, к какому блоку надо перейти
-    * dn - определяем положение блока на странице
-    */
-
-    $('html, body').animate({scrollTop: dn}, 1000);
-
-    /*
-    * 1000 скорость перехода в миллисекундах
-    */
-  });
-});
+// $(function(){
+//   $('a[href^="#"]').on('click', function(event) {
+//     // отменяем стандартное действие
+//     event.preventDefault();
+//
+//     var sc = $(this).attr("href"),
+//         dn = $(sc).offset().top;
+//     /*
+//     * sc - в переменную заносим информацию о том, к какому блоку надо перейти
+//     * dn - определяем положение блока на странице
+//     */
+//
+//     $('html, body').animate({scrollTop: dn}, 1000);
+//
+//     /*
+//     * 1000 скорость перехода в миллисекундах
+//     */
+//   });
+// });
 // $("#randSearch").click(function () {
 //     $('#outputFilm').css('display','none');
 //     $('#loader').css('display','inline-block');
